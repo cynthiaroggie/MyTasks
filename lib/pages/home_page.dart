42bd86a62536273251713aef177d4808e1fa6ee1
cynthiaroggie <cynthia.ruguru.cr@gmail.com>
2022-04-14 14:07:51 +0300

    include stateful widgets and create an empty scaffold

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePage();
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  _HomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
