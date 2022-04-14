b7e52c00183e514720075bde8fdd6f93a362a08a
cynthiaroggie <cynthia.ruguru.cr@gmail.com>
2022-04-14 14:34:11 +0300

    add appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -10,9 +12,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
   @override
   _HomePageState();
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: Text(
+          "My Tasks",
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+    );
   }
 }
